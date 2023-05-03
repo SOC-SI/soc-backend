69e87bae4e85d805fde719b0905a7e8e86e6e9f7
mininus2 <marko.kricej@student.um.si>
2023-05-03 21:19:29 +0200

    parse to dataclass added

--- main.py
+++ main.py
@@ -1,21 +1,52 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+from dataclasses import dataclass
 
 
-def getItemText(item):
-    pattern = r"<.*?>"
-    content = re.sub(pattern, "", item)
+@dataclass
+class ShopItem:
+    znamka: str
+    vrsta: str
+    kolicina: str
+    cena: str
+
+
+def getItemText(items):
+    content = []
+    for item in items:
+        pattern = r"<.*?>"
+        res = re.sub(pattern, "", item)
+        #res = res.replace("\n", "")
+        content.append(res)
     return content
+
+
+def parseStringToDataclass(string):
+    splits = string.split('\n')
+    item = ShopItem("", "", "", "")
+    item.znamka = splits[0]
+    ss = splits[1].split(",", 1)
+    item.vrsta = ss[0]
+    item.kolicina = ss[1]
+    found = False
+    for s in splits:
+        if '€' in s and not found:
+            item.cena = s
+            found = True
+    return item
 
 
 url = "https://www.hofer.si/sl/sortiment/novo.html"
 response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
 items = soup.find_all("div", "item")
+count = 0
+
 for item in items:
     ps = item.find_all("p")
     resultStrings = [str(p) for p in ps]
-    for string in resultStrings:
-        rx = getItemText(string)
-        print(rx)
+    res = getItemText(resultStrings)
+    for r in res:
+        shopitem = parseStringToDataclass(r)
+        print(shopitem)
